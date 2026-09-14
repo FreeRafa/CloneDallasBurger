7dabf167be7153052822ef6b2d01f07e43e609a2
Rafael Velloso <rafavelloso2@gmail.com>
2026-09-14 22:40:27 +0100

    feat: add Categoria entity configuration

---
+++ CloneDallasBurger/Infraestrutura/Configuracao/CategoriaConfiguracao.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using CloneDallasBurger.Modelo.Entidades;
+
+namespace CloneDallasBurger.Infraestrutura.Configuracao
+{
+    public class CategoriaConfiguracao : IEntityTypeConfiguration<Categoria>
+    {
+        public void Configure(EntityTypeBuilder<Categoria> builder) 
+        {
+            builder.ToTable("Categoria");
+            builder.HasKey(c => c.CategoriaId);
+
+            builder.Property(c => c.Nome)
+                .HasColumnName("Nome")
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.HasIndex(c => c.Nome)
+                .IsUnique();
+
+            builder.Property(c => c.Ordem)
+                .HasDefaultValue(0);
+        }
+    }
+}
+
+
