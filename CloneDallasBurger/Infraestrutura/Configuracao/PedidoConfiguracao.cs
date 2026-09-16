a0da8c190b67fa1b011a83dc251bd2160e129ec0
Rafael Velloso <rafavelloso2@gmail.com>
2026-09-16 22:59:49 +0100

    feat: implementado PedidoConfiguracao e ProdutoConfiguracao

---
+++ CloneDallasBurger/Infraestrutura/Configuracao/PedidoConfiguracao.cs
@@ -0,0 +1,60 @@
+﻿using CloneDallasBurger.Modelo.Entidades;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CloneDallasBurger.Infraestrutura.Configuracao
+{
+    public class PedidoConfiguracao : IEntityTypeConfiguration<Pedido>
+    {
+        public void Configure(EntityTypeBuilder<Pedido> builder) 
+        {
+            builder.ToTable("Pedido");
+
+            builder.HasKey(p => p.PedidoId);
+
+            builder.Property(p => p.NumeroPessoas)
+                .IsRequired();
+
+            builder.Property(p => p.Status)
+                .HasMaxLength(20)
+                .IsRequired();
+
+            builder.Property(p => p.DataAbertura)
+                .HasColumnType("datetime")
+                .IsRequired();
+
+            builder.Property(p => p.DataFechamento)
+                .HasColumnType("datetime")
+                .IsRequired();
+
+            builder.Property(p => p.FormaPagamento)
+                .IsRequired();
+
+            builder.Property(p => p.ValorTotal)
+                .HasPrecision(8, 2)
+                .IsRequired();
+
+            builder.Property(p => p.ValorPorPessoa)
+                .HasPrecision(8, 2)
+                .IsRequired();
+
+            builder.Property(p => p.Observacao)
+                .IsRequired();
+
+            builder.HasOne(p => p.Mesa)
+                .WithMany()
+                .HasForeignKey(p => p.MesaId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(p => p.Funcionario)
+                .WithMany()
+                .HasForeignKey(p => p.FuncionarioId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+
+        }
+    }
+}
---
+++ CloneDallasBurger/Infraestrutura/Configuracao/ProdutoConfiguracao.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using CloneDallasBurger.Modelo.Entidades;
+
+namespace CloneDallasBurger.Infraestrutura.Configuracao
+{
+    public class ProdutoConfiguracao : IEntityTypeConfiguration<Produto>
+    {
+        public void Configure(EntityTypeBuilder<Produto> builder) 
+        {
+            builder.ToTable("Produto");
+             
+
+            builder.HasKey(p => p.ProdutoId);
+
+            builder.Property(p => p.Nome)
+                .HasMaxLength(100)
+                .IsRequired(false);
+
+            builder.Property(p => p.Descricao)
+                .HasMaxLength(500)
+                .IsRequired();
+
+            builder.Property(p => p.Preco)
+                .HasPrecision(6, 2)
+                .IsRequired();
+
+            builder.Property(p => p.Vegano)
+                .IsRequired();
+
+            builder.Property(p => p.PermiteTrocaProteina)
+                .IsRequired();
+
+            builder.Property(p => p.Disponivel)
+                .IsRequired();
+
+            builder.HasOne(p => p.Categoria)
+                .WithMany()
+                .HasForeignKey(p => p.CategoriaId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }  
+    }
+}
